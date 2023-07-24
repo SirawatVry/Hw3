cb14c69e7196da9a421d417490477511210b3c28
SirawatVry <139094315+SirawatVry@users.noreply.github.com>
2023-07-24 16:33:00 +0700

    Add files via upload

---
+++ Hw2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+  {
+	int stq,ndq,rdq,mid,final;
+	float Totalq,Total;
+	cout<<"--------QUIZZES--------"<<endl;
+	cout<<"Enter first quizz(10) :";
+	cin>>stq;
+	cout<<"Enter second quizz(10):";
+	cin>>ndq;
+	cout<<"Enter third quizz(10) :";
+	cin>>rdq;
+	cout<<"-------MID TERM-------"<<endl;
+	cout<<"Enter mid-term(40):";
+	cin>>mid;
+	cout<<"---------FINAL---------"<<endl;
+	cout<<"Enter final(50):";
+	cin >>final;
+	Totalq= float (stq+ndq+rdq)/3;
+	cout<<"Quizz Total:"<<Totalq<<endl;
+	cout<<"Mid term:"<<mid<<endl;
+    cout<<"Final:"<<final<<endl;
+	Total=Totalq+mid+final;
+	cout<<"Total:"<<Total<<endl;
+	cout<<"Your score is"<<(Total>=50?" pass":" fail");
+	return(0);
+}
